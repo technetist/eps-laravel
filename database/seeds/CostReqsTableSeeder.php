cc71d956149a31223a779475045ea3ad3338518e
vagrant <vagrant@homestead>
2017-11-08 16:04:03 +0000

    added cost req seeder

---
+++ database/seeds/CostReqsTableSeeder.php
@@ -0,0 +1,886 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use Illuminate\Database\Eloquent\Model;
+
+class CostReqsTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        DB::table('cost_reqs')->insert( [
+            'amount'=>'2',
+            'orderID'=>'1',
+            'product'=>'E1',
+            'time'=>'191'
+        ] );
+
+
+
+        DB::table('cost_reqs')->insert( [
+            'amount'=>'1',
+            'orderID'=>'2',
+            'product'=>'E0',
+            'time'=>'214'
+        ] );
+
+
+
+        DB::table('cost_reqs')->insert( [
+            'amount'=>'1',
+            'orderID'=>'3',
+            'product'=>'E0',
+            'time'=>'227'
+        ] );
+
+
+
+        DB::table('cost_reqs')->insert( [
+            'amount'=>'2',
+            'orderID'=>'4',
+            'product'=>'E1',
+            'time'=>'242'
+        ] );
+
+
+
+        DB::table('cost_reqs')->insert( [
+            'amount'=>'1',
+            'orderID'=>'5',
+            'product'=>'E2',
+            'time'=>'282'
+        ] );
+
+
+
+        DB::table('cost_reqs')->insert( [
+            'amount'=>'1',
+            'orderID'=>'6',
+            'product'=>'E0',
+            'time'=>'313'
+        ] );
+
+
+
+        DB::table('cost_reqs')->insert( [
+            'amount'=>'2',
+            'orderID'=>'7',
+            'product'=>'E1',
+            'time'=>'314'
+        ] );
+
+
+
+        DB::table('cost_reqs')->insert( [
+            'amount'=>'2',
+            'orderID'=>'8',
+            'product'=>'E1',
+            'time'=>'337'
+        ] );
+
+
+
+        DB::table('cost_reqs')->insert( [
+            'amount'=>'1',
+            'orderID'=>'9',
+            'product'=>'E0',
+            'time'=>'349'
+        ] );
+
+
+
+        DB::table('cost_reqs')->insert( [
+            'amount'=>'2',
+            'orderID'=>'10',
+            'product'=>'E1',
+            'time'=>'363'
+        ] );
+
+
+
+        DB::table('cost_reqs')->insert( [
+            'amount'=>'1',
+            'orderID'=>'11',
+            'product'=>'E0',
+            'time'=>'390'
+        ] );
+
+
+
+        DB::table('cost_reqs')->insert( [
+            'amount'=>'1',
+            'orderID'=>'12',
+            'product'=>'E0',
+            'time'=>'435'
+        ] );
+
+
+
+        DB::table('cost_reqs')->insert( [
+            'amount'=>'2',
+            'orderID'=>'13',
+            'product'=>'E1',
+            'time'=>'443'
+        ] );
+
+
+
+        DB::table('cost_reqs')->insert( [
+            'amount'=>'1',
+            'orderID'=>'14',
+            'product'=>'E0',
+            'time'=>'452'
+        ] );
+
+
+
+        DB::table('cost_reqs')->insert( [
+            'amount'=>'2',
+            'orderID'=>'15',
+            'product'=>'E1',
+            'time'=>'476'
+        ] );
+
+
+
+        DB::table('cost_reqs')->insert( [
+            'amount'=>'2',
+            'orderID'=>'16',
+            'product'=>'E1',
+            'time'=>'496'
+        ] );
+
+
+
+        DB::table('cost_reqs')->insert( [
+            'amount'=>'1',
+            'orderID'=>'17',
+            'product'=>'E0',
+            'time'=>'533'
+        ] );
+
+
+
+        DB::table('cost_reqs')->insert( [
+            'amount'=>'2',
+            'orderID'=>'18',
+            'product'=>'E1',
+            'time'=>'567'
+        ] );
+
+
+
+        DB::table('cost_reqs')->insert( [
+            'amount'=>'1',
+            'orderID'=>'19',
+            'product'=>'E0',
+            'time'=>'569'
+        ] );
+
+
+
+        DB::table('cost_reqs')->insert( [
+            'amount'=>'2',
+            'orderID'=>'20',
+            'product'=>'E1',
+            'time'=>'602'
+        ] );
+
+
+
+        DB::table('cost_reqs')->insert( [
+            'amount'=>'1',
+            'orderID'=>'21',
+            'product'=>'E0',
+            'time'=>'624'
+        ] );
+
+
+
+        DB::table('cost_reqs')->insert( [
+            'amount'=>'1',
+            'orderID'=>'22',
+            'product'=>'E2',
+            'time'=>'630'
+        ] );
+
+
+
+        DB::table('cost_reqs')->insert( [
+            'amount'=>'2',
+            'orderID'=>'23',
+            'product'=>'E1',
+            'time'=>'632'
+        ] );
+
+
+
+        DB::table('cost_reqs')->insert( [
+            'amount'=>'1',
+            'orderID'=>'24',
+            'product'=>'E0',
+            'time'=>'641'
+        ] );
+
+
+
+        DB::table('cost_reqs')->insert( [
+            'amount'=>'2',
+            'orderID'=>'25',
+            'product'=>'E1',
+            'time'=>'712'
+        ] );
+
+
+
+        DB::table('cost_reqs')->insert( [
+            'amount'=>'1',
+            'orderID'=>'26',
+            'product'=>'E0',
+            'time'=>'715'
+        ] );
+
+
+
+        DB::table('cost_reqs')->insert( [
+            'amount'=>'2',
+            'orderID'=>'27',
+            'product'=>'E1',
+            'time'=>'729'
+        ] );
+
+
+
+        DB::table('cost_reqs')->insert( [
+            'amount'=>'1',
+            'orderID'=>'28',
+            'product'=>'E2',
+            'time'=>'765'
+        ] );
+
+
+
+        DB::table('cost_reqs')->insert( [
+            'amount'=>'1',
+            'orderID'=>'29',
+            'product'=>'E0',
+            'time'=>'792'
+        ] );
+
+
+
+        DB::table('cost_reqs')->insert( [
+            'amount'=>'2',
+            'orderID'=>'30',
+            'product'=>'E1',
+            'time'=>'794'
+        ] );
+
+
+
+        DB::table('cost_reqs')->insert( [
+            'amount'=>'1',
+            'orderID'=>'31',
+            'product'=>'E2',
+            'time'=>'815'
+        ] );
+
+
+
+        DB::table('cost_reqs')->insert( [
+            'amount'=>'1',
+            'orderID'=>'32',
+            'product'=>'E0',
+            'time'=>'826'
+        ] );
+
+
+
+        DB::table('cost_reqs')->insert( [
+            'amount'=>'2',
+            'orderID'=>'33',
+            'product'=>'E1',
+            'time'=>'838'
+        ] );
+
+
+
+        DB::table('cost_reqs')->insert( [
+            'amount'=>'1',
+            'orderID'=>'34',
+            'product'=>'E0',
+            'time'=>'862'
+        ] );
+
+
+
+        DB::table('cost_reqs')->insert( [
+            'amount'=>'1',
+            'orderID'=>'35',
+            'product'=>'E0',
+            'time'=>'903'
+        ] );
+
+
+
+        DB::table('cost_reqs')->insert( [
+            'amount'=>'2',
+            'orderID'=>'36',
+            'product'=>'E1',
+            'time'=>'910'
+        ] );
+
+
+
+        DB::table('cost_reqs')->insert( [
+            'amount'=>'1',
+            'orderID'=>'37',
+            'product'=>'E0',
+            'time'=>'919'
+        ] );
+
+
+
+        DB::table('cost_reqs')->insert( [
+            'amount'=>'2',
+            'orderID'=>'38',
+            'product'=>'E1',
+            'time'=>'942'
+        ] );
+
+
+
+        DB::table('cost_reqs')->insert( [
+            'amount'=>'2',
+            'orderID'=>'39',
+            'product'=>'E1',
+            'time'=>'963'
+        ] );
+
+
+
+        DB::table('cost_reqs')->insert( [
+            'amount'=>'1',
+            'orderID'=>'40',
+            'product'=>'E0',
+            'time'=>'1000'
+        ] );
+
+
+
+        DB::table('cost_reqs')->insert( [
+            'amount'=>'2',
+            'orderID'=>'41',
+            'product'=>'E1',
+            'time'=>'1034'
+        ] );
+
+
+
+        DB::table('cost_reqs')->insert( [
+            'amount'=>'1',
+            'orderID'=>'42',
+            'product'=>'E0',
+            'time'=>'1036'
+        ] );
+
+
+
+        DB::table('cost_reqs')->insert( [
+            'amount'=>'2',
+            'orderID'=>'43',
+            'product'=>'E1',
+            'time'=>'1070'
+        ] );
+
+
+
+        DB::table('cost_reqs')->insert( [
+            'amount'=>'1',
+            'orderID'=>'44',
+            'product'=>'E0',
+            'time'=>'1092'
+        ] );
+
+
+
+        DB::table('cost_reqs')->insert( [
+            'amount'=>'1',
+            'orderID'=>'45',
+            'product'=>'E2',
+            'time'=>'1098'
+        ] );
+
+
+
+        DB::table('cost_reqs')->insert( [
+            'amount'=>'2',
+            'orderID'=>'46',
+            'product'=>'E1',
+            'time'=>'1099'
+        ] );
+
+
+
+        DB::table('cost_reqs')->insert( [
+            'amount'=>'1',
+            'orderID'=>'47',
+            'product'=>'E0',
+            'time'=>'1109'
+        ] );
+
+
+
+        DB::table('cost_reqs')->insert( [
+            'amount'=>'2',
+            'orderID'=>'48',
+            'product'=>'E1',
+            'time'=>'1154'
+        ] );
+
+
+
+        DB::table('cost_reqs')->insert( [
+            'amount'=>'1',
+            'orderID'=>'49',
+            'product'=>'E0',
+            'time'=>'1158'
+        ] );
+
+
+
+        DB::table('cost_reqs')->insert( [
+            'amount'=>'2',
+            'orderID'=>'50',
+            'product'=>'E1',
+            'time'=>'1193'
+        ] );
+
+
+
+        DB::table('cost_reqs')->insert( [
+            'amount'=>'1',
+            'orderID'=>'51',
+            'product'=>'E0',
+            'time'=>'1216'
+        ] );
+
+
+
+        DB::table('cost_reqs')->insert( [
+            'amount'=>'1',
+            'orderID'=>'52',
+            'product'=>'E0',
+            'time'=>'1229'
+        ] );
+
+
+
+        DB::table('cost_reqs')->insert( [
+            'amount'=>'2',
+            'orderID'=>'53',
+            'product'=>'E1',
+            'time'=>'1244'
+        ] );
+
+
+
+        DB::table('cost_reqs')->insert( [
+            'amount'=>'1',
+            'orderID'=>'54',
+            'product'=>'E2',
+            'time'=>'1284'
+        ] );
+
+
+
+        DB::table('cost_reqs')->insert( [
+            'amount'=>'1',
+            'orderID'=>'55',
+            'product'=>'E0',
+            'time'=>'1315'
+        ] );
+
+
+
+        DB::table('cost_reqs')->insert( [
+            'amount'=>'2',
+            'orderID'=>'56',
+            'product'=>'E1',
+            'time'=>'1316'
+        ] );
+
+
+
+        DB::table('cost_reqs')->insert( [
+            'amount'=>'2',
+            'orderID'=>'57',
+            'product'=>'E1',
+            'time'=>'1339'
+        ] );
+
+
+
+        DB::table('cost_reqs')->insert( [
+            'amount'=>'1',
+            'orderID'=>'58',
+            'product'=>'E0',
+            'time'=>'1351'
+        ] );
+
+
+
+        DB::table('cost_reqs')->insert( [
+            'amount'=>'2',
+            'orderID'=>'59',
+            'product'=>'E1',
+            'time'=>'1365'
+        ] );
+
+
+
+        DB::table('cost_reqs')->insert( [
+            'amount'=>'1',
+            'orderID'=>'60',
+            'product'=>'E0',
+            'time'=>'1392'
+        ] );
+
+
+
+        DB::table('cost_reqs')->insert( [
+            'amount'=>'1',
+            'orderID'=>'61',
+            'product'=>'E0',
+            'time'=>'1437'
+        ] );
+
+
+
+        DB::table('cost_reqs')->insert( [
+            'amount'=>'2',
+            'orderID'=>'62',
+            'product'=>'E1',
+            'time'=>'1445'
+        ] );
+
+
+
+        DB::table('cost_reqs')->insert( [
+            'amount'=>'1',
+            'orderID'=>'63',
+            'product'=>'E0',
+            'time'=>'1454'
+        ] );
+
+
+
+        DB::table('cost_reqs')->insert( [
+            'amount'=>'2',
+            'orderID'=>'64',
+            'product'=>'E1',
+            'time'=>'1478'
+        ] );
+
+
+
+        DB::table('cost_reqs')->insert( [
+            'amount'=>'2',
+            'orderID'=>'65',
+            'product'=>'E1',
+            'time'=>'1498'
+        ] );
+
+
+
+        DB::table('cost_reqs')->insert( [
+            'amount'=>'1',
+            'orderID'=>'66',
+            'product'=>'E0',
+            'time'=>'1535'
+        ] );
+
+
+
+        DB::table('cost_reqs')->insert( [
+            'amount'=>'2',
+            'orderID'=>'67',
+            'product'=>'E1',
+            'time'=>'1569'
+        ] );
+
+
+
+        DB::table('cost_reqs')->insert( [
+            'amount'=>'1',
+            'orderID'=>'68',
+            'product'=>'E0',
+            'time'=>'1571'
+        ] );
+
+
+
+        DB::table('cost_reqs')->insert( [
+            'amount'=>'2',
+            'orderID'=>'69',
+            'product'=>'E1',
+            'time'=>'1604'
+        ] );
+
+
+
+        DB::table('cost_reqs')->insert( [
+            'amount'=>'1',
+            'orderID'=>'70',
+            'product'=>'E0',
+            'time'=>'1626'
+        ] );
+
+
+
+        DB::table('cost_reqs')->insert( [
+            'amount'=>'1',
+            'orderID'=>'71',
+            'product'=>'E2',
+            'time'=>'1632'
+        ] );
+
+
+
+        DB::table('cost_reqs')->insert( [
+            'amount'=>'2',
+            'orderID'=>'72',
+            'product'=>'E1',
+            'time'=>'1632'
+        ] );
+
+
+
+        DB::table('cost_reqs')->insert( [
+            'amount'=>'1',
+            'orderID'=>'73',
+            'product'=>'E0',
+            'time'=>'1643'
+        ] );
+
+
+
+        DB::table('cost_reqs')->insert( [
+            'amount'=>'2',
+            'orderID'=>'74',
+            'product'=>'E1',
+            'time'=>'1714'
+        ] );
+
+
+
+        DB::table('cost_reqs')->insert( [
+            'amount'=>'1',
+            'orderID'=>'75',
+            'product'=>'E0',
+            'time'=>'1717'
+        ] );
+
+
+
+        DB::table('cost_reqs')->insert( [
+            'amount'=>'2',
+            'orderID'=>'76',
+            'product'=>'E1',
+            'time'=>'1731'
+        ] );
+
+
+
+        DB::table('cost_reqs')->insert( [
+            'amount'=>'1',
+            'orderID'=>'77',
+            'product'=>'E2',
+            'time'=>'1767'
+        ] );
+
+
+
+        DB::table('cost_reqs')->insert( [
+            'amount'=>'1',
+            'orderID'=>'78',
+            'product'=>'E0',
+            'time'=>'1794'
+        ] );
+
+
+
+        DB::table('cost_reqs')->insert( [
+            'amount'=>'2',
+            'orderID'=>'79',
+            'product'=>'E1',
+            'time'=>'1796'
+        ] );
+
+
+
+        DB::table('cost_reqs')->insert( [
+            'amount'=>'2',
+            'orderID'=>'80',
+            'product'=>'E1',
+            'time'=>'1817'
+        ] );
+
+
+
+        DB::table('cost_reqs')->insert( [
+            'amount'=>'1',
+            'orderID'=>'81',
+            'product'=>'E0',
+            'time'=>'1828'
+        ] );
+
+
+
+        DB::table('cost_reqs')->insert( [
+            'amount'=>'2',
+            'orderID'=>'82',
+            'product'=>'E1',
+            'time'=>'1840'
+        ] );
+
+
+
+        DB::table('cost_reqs')->insert( [
+            'amount'=>'1',
+            'orderID'=>'83',
+            'product'=>'E0',
+            'time'=>'1864'
+        ] );
+
+
+
+        DB::table('cost_reqs')->insert( [
+            'amount'=>'1',
+            'orderID'=>'84',
+            'product'=>'E0',
+            'time'=>'1905'
+        ] );
+
+
+
+        DB::table('cost_reqs')->insert( [
+            'amount'=>'2',
+            'orderID'=>'85',
+            'product'=>'E1',
+            'time'=>'1912'
+        ] );
+
+
+
+        DB::table('cost_reqs')->insert( [
+            'amount'=>'1',
+            'orderID'=>'86',
+            'product'=>'E0',
+            'time'=>'1921'
+        ] );
+
+
+
+        DB::table('cost_reqs')->insert( [
+            'amount'=>'2',
+            'orderID'=>'87',
+            'product'=>'E1',
+            'time'=>'1944'
+        ] );
+
+
+
+        DB::table('cost_reqs')->insert( [
+            'amount'=>'2',
+            'orderID'=>'88',
+            'product'=>'E1',
+            'time'=>'1965'
+        ] );
+
+
+
+        DB::table('cost_reqs')->insert( [
+            'amount'=>'1',
+            'orderID'=>'89',
+            'product'=>'E0',
+            'time'=>'2002'
+        ] );
+
+
+
+        DB::table('cost_reqs')->insert( [
+            'amount'=>'2',
+            'orderID'=>'90',
+            'product'=>'E1',
+            'time'=>'2036'
+        ] );
+
+
+
+        DB::table('cost_reqs')->insert( [
+            'amount'=>'1',
+            'orderID'=>'91',
+            'product'=>'E0',
+            'time'=>'2038'
+        ] );
+
+
+
+        DB::table('cost_reqs')->insert( [
+            'amount'=>'2',
+            'orderID'=>'92',
+            'product'=>'E1',
+            'time'=>'2072'
+        ] );
+
+
+
+        DB::table('cost_reqs')->insert( [
+            'amount'=>'1',
+            'orderID'=>'93',
+            'product'=>'E0',
+            'time'=>'2094'
+        ] );
+
+
+
+        DB::table('cost_reqs')->insert( [
+            'amount'=>'1',
+            'orderID'=>'94',
+            'product'=>'E2',
+            'time'=>'2100'
+        ] );
+
+
+
+        DB::table('cost_reqs')->insert( [
+            'amount'=>'2',
+            'orderID'=>'95',
+            'product'=>'E1',
+            'time'=>'2101'
+        ] );
+
+
+
+        DB::table('cost_reqs')->insert( [
+            'amount'=>'1',
+            'orderID'=>'96',
+            'product'=>'E0',
+            'time'=>'2111'
+        ] );
+
+
+
+        DB::table('cost_reqs')->insert( [
+            'amount'=>'2',
+            'orderID'=>'97',
+            'product'=>'E1',
+            'time'=>'2156'
+        ] );
+    }
+}
