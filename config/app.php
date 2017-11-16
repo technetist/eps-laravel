d3618bd1ed0bfaad44c0aad43b8fe61f5867256b
vagrant <vagrant@homestead>
2017-11-16 12:44:32 +0000

    added debug bar and username login

--- config/app.php
+++ config/app.php
@@ -168,6 +168,7 @@ return [
          * Package Service Providers...
          */
         Laravel\Passport\PassportServiceProvider::class,
+        Barryvdh\Debugbar\ServiceProvider::class,
 
         /*
          * Application Service Providers...
@@ -226,6 +227,7 @@ return [
         'URL' => Illuminate\Support\Facades\URL::class,
         'Validator' => Illuminate\Support\Facades\Validator::class,
         'View' => Illuminate\Support\Facades\View::class,
+        'Debugbar' => Barryvdh\Debugbar\Facade::class,
 
     ],
 
