addb378503964c6374edc99f2b4c1c8f45dc322e
Adrien Maranville <adrien@maranville.io>
2017-11-20 14:36:16 +0100

    set up socketio test

---
+++ resources/views/socketio.blade.php
@@ -0,0 +1,17 @@
+@extends('layouts.socketio_master')
+
+@section('content')
+    <p id="power">0</p>
+@stop
+
+@section('footer')
+    <script src="{ { asset('js/socket.io.js') } }"></script>
+    <script>
+
+        var socket = io('http://192.168.10.10:3000');
+        socket.on("test-channel:App\\Events\\SocketMessageEvent", function(message){
+
+            $('#power').text(parseInt($('#power').text()) + parseInt(message.data.power));
+        });
+    </script>
+@stop
