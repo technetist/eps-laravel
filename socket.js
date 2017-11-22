5306bfae1397e9685b01a31429ae719866569391
Adrien Maranville <adrien@maranville.io>
2017-11-22 15:20:38 +0100

    added socket.io

--- socket.js
+++ socket.js
@@ -1,19 +1,65 @@
-var app = require('express')();
-var http = require('http').Server(app);
-var io = require('socket.io')(http);
-var Redis = require('ioredis');
-var redis = new Redis();
-redis.subscribe('test-channel', function(err, count) {
-});
-redis.on('message', function(channel, message) {
-    console.log('Message Received: ' + message);
-    message = JSON.parse(message);
-    io.emit(channel + ':' + message.event, message.data);
-});
-redis.on('connection', function(client) {
-    console.log('Connected');
-    io.emit("Client Connected");
-});
-http.listen(3000, function(){
-    console.log('Listening on Port 3000');
-});
+const SERVER_PORT = 8000
+
+var express = require('express');
+var app = express();
+var server = require('http').Server(app);
+
+var io = require('socket.io').listen(server);
+var redis = require('redis')
+// var ioredis = require('socket.io-redis')
+
+var sub = redis.createClient()
+
+sub.on('error', function (error) {
+    console.log('ERROR ' + error)
+})
+
+sub.on('subscribe', function (channel, count) {
+    console.log('SUBSCRIBE', channel, count)
+})
+
+// Handle messages from channels we're subscribed to
+sub.on('message', function (channel, payload) {
+    console.log('INCOMING MESSAGE', channel, payload)
+
+    payload = JSON.parse(payload)
+
+    // Merge channel into payload
+    payload.data._channel = channel
+
+    // Send the data through to any client in the channel room (!)
+    // (i.e. server room, usually being just the one user)
+    io.sockets.in(channel).emit(payload.event, payload.data)
+})
+
+/*
+ * Server
+ */
+
+// Start listening for incoming client connections
+io.sockets.on('connection', function (socket) {
+
+    console.log('NEW CLIENT CONNECTED')
+
+    socket.on('subscribe-to-channel', function (data) {
+        console.log('SUBSCRIBE TO CHANNEL', data)
+
+        // Subscribe to the Redis channel using our global subscriber
+        sub.subscribe(data.channel)
+
+        // Join the (somewhat local) server room for this channel. This
+        // way we can later pass our channel events right through to
+        // the room instead of broadcasting them to every client.
+        socket.join(data.channel)
+    })
+
+    socket.on('disconnect', function () {
+        console.log('DISCONNECT')
+    })
+
+})
+
+// Start listening for client connections
+server.listen(SERVER_PORT, function () {
+    console.log('Listening to incoming client connections on port ' + SERVER_PORT)
+})
