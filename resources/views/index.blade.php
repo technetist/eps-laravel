f1c34279fc7e9f3373f9a687b898ce8b30ba0d17
Adrien Maranville <adrien@maranville.io>
2017-11-22 13:57:54 +0100

    added vue and socket functions

---
+++ resources/views/index.blade.php
@@ -0,0 +1,17 @@
+@extends('layouts.master')
+@section('content')
+    <div class="container">
+        <table class="table table-bordered">
+            <tr>
+                <th>Name</th>
+                <th>Email</th>
+                <th>Created At</th>
+            </tr>
+            <tr>
+                <td>Adrien</td>
+                <td>Something</td>
+                <td>12</td>
+            </tr>
+        </table>
+    </div>
+@endsection
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -75,6 +75,7 @@
     </div>
 
     <!-- Scripts -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.4/jquery.min.js"></script>
     <script src="{{ mix('/js/app.js') }}"></script>
 </body>
 </html>
