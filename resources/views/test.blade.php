001853ba0ea22ed337a0512c737a297e849af125
Adrien Maranville <adrien@maranville.io>
2017-11-30 14:36:13 +0100

    fixed socket.io.js problem

--- resources/views/test.blade.php
+++ resources/views/test.blade.php
@@ -5,7 +5,7 @@
 @stop
 
 @section('footer')
-    <script src="{ { asset('js/socket.io.js') } }"></script>
+    <script src="{{ asset('js/socket.io.js') }}"></script>
     <script>
 
         var socket = io('http://192.168.10.10:8000');
