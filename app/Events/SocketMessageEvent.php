f0be2c651f2c0a6dfb2fbfb3841123f5ad126737
Adrien Maranville <adrien@maranville.io>
2017-11-30 14:44:30 +0100

    added broadcast message

--- app/Events/SocketMessageEvent.php
+++ app/Events/SocketMessageEvent.php
@@ -1,7 +1,5 @@
 <?php
-
 namespace App\Events;
-
 use Illuminate\Broadcasting\Channel;
 use Illuminate\Queue\SerializesModels;
 use Illuminate\Broadcasting\PrivateChannel;
@@ -9,33 +7,34 @@ use Illuminate\Broadcasting\PresenceChannel;
 use Illuminate\Foundation\Events\Dispatchable;
 use Illuminate\Broadcasting\InteractsWithSockets;
 use Illuminate\Contracts\Broadcasting\ShouldBroadcast;
-
 class SocketMessageEvent implements ShouldBroadcast
 {
     use Dispatchable, InteractsWithSockets, SerializesModels;
-
     /**
      * Create a new event instance.
      *
      * @return void
      */
-
-    public $data;
-
+    public $from;
+    public $to;
+    public $message;
     public function __construct()
     {
-        $this->data = array(
-            'power'=> '10'
-        );
+        $this->from = 'Bond';
+        $this->to = 'Bean';
+        $this->message = 'You don\'t say??';
     }
-
-    /**
-     * Get the channels the event should broadcast on.
-     *
-     * @return \Illuminate\Broadcasting\Channel|array
-     */
     public function broadcastOn()
     {
-        return ['test-channel'];
+        return ['ppc-game-communication-broadcast'];
+    }
+    /**
+     * Get the broadcast event name.
+     *
+     * @return string
+     */
+    public function broadcastAs()
+    {
+        return 'messages.new';
     }
 }
