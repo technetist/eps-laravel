7312973a5a6ae06791dae9f134c5733d65c4c2e0
Adrien Maranville <adrien@maranville.io>
2017-12-11 13:37:50 +0100

    add csrf token

--- resources/views/layouts/vue.blade.php
+++ resources/views/layouts/vue.blade.php
@@ -2,6 +2,7 @@
 <html lang="en">
 <head>
     <meta charset="UTF-8">
+    <meta name="csrf-token" content="{{ csrf_token() }}">
     <title>Vue page</title>
 
     <link href="{{ mix('/css/app.css') }}" rel="stylesheet">
