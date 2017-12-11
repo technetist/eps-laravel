0681c59b70478562c88e34c5f6a98f55691d94a3
Adrien Maranville <adrien@maranville.io>
2017-12-11 13:31:48 +0100

    client

--- resources/assets/js/client.js
+++ resources/assets/js/client.js
@@ -16,6 +16,10 @@ socket.on('connect', function () {
         console.log('disconnect')
     })
 
+    socket.emit('whoworkin', function () {
+        console.log('sup loser')
+    })
+
     // Kick it off
     // Can be any channel. For private channels, Laravel should pass it upon page load (or given by another user).
     socket.emit('subscribe-to-channel', {channel: PRIVATE_CHANNEL})
