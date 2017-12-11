9030e165b999b03043c948d01130640de708930b
Adrien Maranville <adrien@maranville.io>
2017-12-11 13:54:51 +0100

    event trigger edit 2

--- app/Events/SocketMessageEvent.php
+++ app/Events/SocketMessageEvent.php
@@ -20,7 +20,7 @@ class SocketMessageEvent implements ShouldBroadcast
     public $status;
     public function __construct()
     {
-        $active = DB::table('stats')->where('status','active');
+        $active = DB::table('stats')->where('status','active')->get();
         $this->status = $active;
     }
     public function broadcastOn()
