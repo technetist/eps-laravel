4b32c9292bab0994df9e9df72373fe1b694b1b1e
Adrien Maranville <adrien@maranville.io>
2017-12-12 16:34:24 +0100

    fixing js

--- resources/views/test.blade.php
+++ resources/views/test.blade.php
@@ -7,7 +7,7 @@
 @stop
 
 @section('footer')
-    <script src="{{ mix('/js/app.js') }}"></script>
+    <script src="{{ mix('/js/chartapp.js') }}"></script>
     <script>var app_ip = '{{ env("MIX_APP_IP") }}';</script>
 
     <script src="{{ mix('/js/client.js') }}"></script>
