9634e8674a6783893637fa021f7d673d0897be00
italianman1 <a.avitabile@student.avans.nl>
2017-12-15 13:32:08 +0100

    Made a search bar and changed the analyse controller

--- app/Http/Controllers/AnalyseController.php
+++ app/Http/Controllers/AnalyseController.php
@@ -3,14 +3,25 @@
 namespace App\Http\Controllers;
 
 
+use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
 
 class AnalyseController
 {
-    public function index()
+    public function index(Request $request)
     {
-        $sessions = DB::table('sessions')->get();
+        $allSessions = DB::table('sessions')->get();
         $inventories = DB::table('inventories')->get();
+        $search = $request->name;
+
+        if($search == '' or $search == null ){
+            $sessions = $allSessions;
+        }
+
+        else{
+            $sessions = DB::table('sessions')->select('sessionName')->where('sessionName', 'like', "$search");
+
+        }
 
         return view('analyse', compact('sessions', 'inventories'));
     }
