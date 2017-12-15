527c480d8015cf3ceb731f30f67ed6a8a4440485
italianman1 <a.avitabile@student.avans.nl>
2017-12-15 13:30:12 +0100

    fixed the timer

--- resources/assets/js/vueclient.js
+++ resources/assets/js/vueclient.js
@@ -1,17 +1,18 @@
 const PRIVATE_CHANNEL = 'ppc-game-communication-broadcast'
 var io = require('socket.io-client')
-var socket = io.connect('http://' + app_ip + ':8000');
-
+var socket = io.connect('http://'+ app_ip +':8000');
 
 socket.on('connect', function () {
     console.log('CONNECT')
 
     document.getElementById("start").addEventListener("click", function () {
+        this.setAttribute("disabled", true)
         socket.emit("start")
-        console.log("start clicked!")
+        console.log("clicking!")
     })
 
     document.getElementById("reset").addEventListener("click", function () {
+        document.getElementById("start").removeAttribute("disabled");
         socket.emit("reset")
         console.log("reset clicked!")
     })
@@ -25,7 +26,12 @@ socket.on('connect', function () {
     })
 
     socket.on('timer', function(data) {
-        document.getElementById("counter").innerHTML = data.time;
+        console.log('started timer');
+        let minutes = Math.floor(data.time / 60);
+        let seconds = data.time - minutes * 60;
+        let minutes2 = (minutes < 10 ? '0' : '') + minutes;
+        let seconds2 = (seconds < 10 ? '0' : '') + seconds;
+        document.getElementById("counter").innerHTML = minutes2+':'+seconds2;
     });
 
     socket.emit('subscribe-to-channel', {channel: PRIVATE_CHANNEL})
