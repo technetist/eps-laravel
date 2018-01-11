0710908b0b66ffa32a7d84478a0dd8e7458f656c
Adrien Maranville <adrien@maranville.io>
2018-01-11 13:25:23 +0100

    created basics for pushSession

--- database_manager.js
+++ database_manager.js
@@ -16,19 +16,27 @@ function db_connection(crud, table) {
 }
 
 function db_push() {
-    connection.query('INSERT INTO' + table, function (err, rows, fields) {
-        if (err) throw err
 
-        console.log(rows)
-    })
 }
 
 function db_pull() {
 
 }
 
-function pushSession(){
+function pushSession(utiliaztion_array,
+                     total_time,
+                     mrp_perameters_array,
+                     averages_array, lead_time,
+                     planning_algorithm,
+                     service_level,
+                     session_name,
+                     time_per_piece,
+                     time_per_piece_per_machine_array){
+    connection.query('INSERT INTO sessions () VALUES ()', function (err, rows, fields) {
+        if (err) throw err
 
+        console.log(rows)
+    })
 }
 
 function getParameters() {
