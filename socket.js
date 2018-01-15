5c0c963fafdbcd7b329cea2e3a19cf18fb8f3ac4
Adrien Maranville <adrien@maranville.io>
2018-01-15 15:35:45 +0100

    added debug message

--- socket.js
+++ socket.js
@@ -230,7 +230,7 @@ io.sockets.on('connection', function (socket) {
         mStateUpdater(data.machine,'idle');
         if(data.machine == "machine5"){
             WIP -= data.amount;
-
+            console.log(data.machine + " finished working")
             if(data.product === 'E0'){
                 FGI.E0 += data.amount;
             }else if(data.product === 'E1'){
